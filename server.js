ebb9d2ff5a7696b6cfe7f528d0bfb9bf6066ca48
Ec2speed <130516355+Ec2speed@users.noreply.github.com>
2026-04-09 14:17:56 +0800

    Update server.js

--- server.js
+++ server.js
@@ -76,6 +76,8 @@ app.put("/order/:id", async (req, res) => {
 
 app.use("/uploads", express.static("uploads"));
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Server running...");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
