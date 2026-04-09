1d502560626a7e4ea0b6440a559862480b16f3bb
Ec2speed <130516355+Ec2speed@users.noreply.github.com>
2026-04-09 14:58:05 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server running on port ${PORT}`);
+});
 // File Upload Setup
 const storage = multer.diskStorage({
   destination: "uploads/",
@@ -63,8 +71,6 @@ app.put("/order/:id", async (req, res) => {
 
 app.use("/uploads", express.static("uploads"));
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server running...");
 });
